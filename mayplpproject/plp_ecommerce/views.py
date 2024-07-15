b70741a39218e01242b3a086c0664bdc20ce5cd5
Pauline Sanzua <paulinesanzua@gmail.com>
2024-07-15 14:48:35 +0300

    Add files via upload

--- mayplpproject/plp_ecommerce/views.py
+++ mayplpproject/plp_ecommerce/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-     return HttpResponse("<h2>my first webpage with python Django</h2>")
+     return HttpResponse("my first webpage with python Django")
